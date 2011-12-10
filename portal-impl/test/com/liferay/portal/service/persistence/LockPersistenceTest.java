zxdgoal/liferay-portal

commit ed666c4ec49fda499ef90bc5ce98aa30d7226dcb

LPS-23963 Added unique index on lock table

portal-impl/src/com/liferay/portal/service.xml
@@ -878,7 +878,7 @@
 		<finder name="LtExpirationDate" return-type="Collection">
 			<finder-column name="expirationDate" comparator="&lt;" />
 		</finder>
-		<finder name="C_K" return-type="Lock">
+		<finder name="C_K" return-type="Lock" unique="true">
 			<finder-column name="className" />
 			<finder-column name="key" />
 		</finder>

sql/indexes.sql
@@ -394,7 +394,7 @@ create index IX_C5D69B24 on LayoutSetPrototype (uuid_);
 
 create index IX_2932DD37 on ListType (type_);
 
-create index IX_228562AD on Lock_ (className, key_);
+create unique index IX_228562AD on Lock_ (className, key_);
 create index IX_E3F1286B on Lock_ (expirationDate);
 create index IX_13C5CD3A on Lock_ (uuid_);
 

commit 7d10d33b1b83ba70508a3e4f3a43c159a48676aa

LPS-23963 Fix bug introduced in ed666c4ec49fda499ef90bc5ce98aa30d7226dcb

portal-impl/src/com/liferay/portal/model/impl/LockModelImpl.java
@@ -85,7 +85,8 @@ public class LockModelImpl extends BaseModelImpl<Lock> implements LockModel {
 	public static long CLASSNAME_COLUMN_BITMASK = 1L;
 	public static long EXPIRATIONDATE_COLUMN_BITMASK = 2L;
 	public static long KEY_COLUMN_BITMASK = 4L;
-	public static long UUID_COLUMN_BITMASK = 8L;
+	public static long OWNER_COLUMN_BITMASK = 8L;
+	public static long UUID_COLUMN_BITMASK = 16L;
 	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(com.liferay.portal.util.PropsUtil.get(
 				"lock.expiration.time.com.liferay.portal.model.Lock"));
 
@@ -246,7 +247,17 @@ public class LockModelImpl extends BaseModelImpl<Lock> implements LockModel {
 	}
 
 	public void setOwner(String owner) {
+		_columnBitmask |= OWNER_COLUMN_BITMASK;
+
+		if (_originalOwner == null) {
+			_originalOwner = _owner;
+		}
+
 		_owner = owner;
+	}
+
+	public String getOriginalOwner() {
+		return GetterUtil.getString(_originalOwner);
 	}
 
 	public boolean getInheritable() {
@@ -383,6 +394,8 @@ public class LockModelImpl extends BaseModelImpl<Lock> implements LockModel {
 		lockModelImpl._originalClassName = lockModelImpl._className;
 
 		lockModelImpl._originalKey = lockModelImpl._key;
+
+		lockModelImpl._originalOwner = lockModelImpl._owner;
 
 		lockModelImpl._originalExpirationDate = lockModelImpl._expirationDate;
 
@@ -567,6 +580,7 @@ public class LockModelImpl extends BaseModelImpl<Lock> implements LockModel {
 	private String _key;
 	private String _originalKey;
 	private String _owner;
+	private String _originalOwner;
 	private boolean _inheritable;
 	private Date _expirationDate;
 	private Date _originalExpirationDate;

portal-impl/src/com/liferay/portal/service.xml
@@ -878,9 +878,14 @@
 		<finder name="LtExpirationDate" return-type="Collection">
 			<finder-column name="expirationDate" comparator="&lt;" />
 		</finder>
+		<finder name="C_K" return-type="Lock">
+			<finder-column name="className" />
+			<finder-column name="key" />
+		</finder>
 		<finder name="C_K" return-type="Lock" unique="true">
 			<finder-column name="className" />
 			<finder-column name="key" />
+			<finder-column name="owner" />
 		</finder>
 	</entity>
 	<entity name="MembershipRequest" local-service="true" remote-service="true">

portal-impl/src/com/liferay/portal/service/persistence/LockPersistenceImpl.java
@@ -123,6 +123,23 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 			LockModelImpl.FINDER_CACHE_ENABLED, Long.class,
 			FINDER_CLASS_NAME_LIST_WITHOUT_PAGINATION, "countByC_K",
 			new String[] { String.class.getName(), String.class.getName() });
+	public static final FinderPath FINDER_PATH_FETCH_BY_C_K = new FinderPath(LockModelImpl.ENTITY_CACHE_ENABLED,
+			LockModelImpl.FINDER_CACHE_ENABLED, LockImpl.class,
+			FINDER_CLASS_NAME_ENTITY, "fetchByC_K",
+			new String[] {
+				String.class.getName(), String.class.getName(),
+				String.class.getName()
+			},
+			LockModelImpl.CLASSNAME_COLUMN_BITMASK |
+			LockModelImpl.KEY_COLUMN_BITMASK |
+			LockModelImpl.OWNER_COLUMN_BITMASK);
+	public static final FinderPath FINDER_PATH_COUNT_BY_C_K = new FinderPath(LockModelImpl.ENTITY_CACHE_ENABLED,
+			LockModelImpl.FINDER_CACHE_ENABLED, Long.class,
+			FINDER_CLASS_NAME_LIST_WITHOUT_PAGINATION, "countByC_K",
+			new String[] {
+				String.class.getName(), String.class.getName(),
+				String.class.getName()
+			});
 	public static final FinderPath FINDER_PATH_WITH_PAGINATION_FIND_ALL = new FinderPath(LockModelImpl.ENTITY_CACHE_ENABLED,
 			LockModelImpl.FINDER_CACHE_ENABLED, LockImpl.class,
 			FINDER_CLASS_NAME_LIST_WITHOUT_PAGINATION, "findAll", new String[0]);
@@ -144,6 +161,10 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 
 		FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
 			new Object[] { lock.getClassName(), lock.getKey() }, lock);
+
+		FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
+			new Object[] { lock.getClassName(), lock.getKey(), lock.getOwner() },
+			lock);
 
 		lock.resetOriginalValues();
 	}
@@ -219,6 +240,9 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 	protected void clearUniqueFindersCache(Lock lock) {
 		FinderCacheUtil.removeResult(FINDER_PATH_FETCH_BY_C_K,
 			new Object[] { lock.getClassName(), lock.getKey() });
+
+		FinderCacheUtil.removeResult(FINDER_PATH_FETCH_BY_C_K,
+			new Object[] { lock.getClassName(), lock.getKey(), lock.getOwner() });
 	}
 
 	/**
@@ -394,6 +418,10 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 		if (isNew) {
 			FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
 				new Object[] { lock.getClassName(), lock.getKey() }, lock);
+
+			FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
+				new Object[] { lock.getClassName(), lock.getKey(), lock.getOwner() },
+				lock);
 		}
 		else {
 			if ((lockModelImpl.getColumnBitmask() &
@@ -409,6 +437,29 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 
 				FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
 					new Object[] { lock.getClassName(), lock.getKey() }, lock);
+			}
+
+			if ((lockModelImpl.getColumnBitmask() &
+					FINDER_PATH_FETCH_BY_C_K.getColumnBitmask()) != 0) {
+				Object[] args = new Object[] {
+						lockModelImpl.getOriginalClassName(),
+						
+						lockModelImpl.getOriginalKey(),
+						
+						lockModelImpl.getOriginalOwner()
+					};
+
+				FinderCacheUtil.removeResult(FINDER_PATH_COUNT_BY_C_K, args);
+				FinderCacheUtil.removeResult(FINDER_PATH_FETCH_BY_C_K, args);
+
+				FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
+					new Object[] {
+						lock.getClassName(),
+						
+					lock.getKey(),
+						
+					lock.getOwner()
+					}, lock);
 			}
 		}
 
@@ -1415,6 +1466,195 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 	}
 
 	/**
+	 * Returns the lock where className = &#63; and key = &#63; and owner = &#63; or throws a {@link com.liferay.portal.NoSuchLockException} if it could not be found.
+	 *
+	 * @param className the class name
+	 * @param key the key
+	 * @param owner the owner
+	 * @return the matching lock
+	 * @throws com.liferay.portal.NoSuchLockException if a matching lock could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
+	public Lock findByC_K(String className, String key, String owner)
+		throws NoSuchLockException, SystemException {
+		Lock lock = fetchByC_K(className, key, owner);
+
+		if (lock == null) {
+			StringBundler msg = new StringBundler(8);
+
+			msg.append(_NO_SUCH_ENTITY_WITH_KEY);
+
+			msg.append("className=");
+			msg.append(className);
+
+			msg.append(", key=");
+			msg.append(key);
+
+			msg.append(", owner=");
+			msg.append(owner);
+
+			msg.append(StringPool.CLOSE_CURLY_BRACE);
+
+			if (_log.isWarnEnabled()) {
+				_log.warn(msg.toString());
+			}
+
+			throw new NoSuchLockException(msg.toString());
+		}
+
+		return lock;
+	}
+
+	/**
+	 * Returns the lock where className = &#63; and key = &#63; and owner = &#63; or returns <code>null</code> if it could not be found. Uses the finder cache.
+	 *
+	 * @param className the class name
+	 * @param key the key
+	 * @param owner the owner
+	 * @return the matching lock, or <code>null</code> if a matching lock could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
+	public Lock fetchByC_K(String className, String key, String owner)
+		throws SystemException {
+		return fetchByC_K(className, key, owner, true);
+	}
+
+	/**
+	 * Returns the lock where className = &#63; and key = &#63; and owner = &#63; or returns <code>null</code> if it could not be found, optionally using the finder cache.
+	 *
+	 * @param className the class name
+	 * @param key the key
+	 * @param owner the owner
+	 * @param retrieveFromCache whether to use the finder cache
+	 * @return the matching lock, or <code>null</code> if a matching lock could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
+	public Lock fetchByC_K(String className, String key, String owner,
+		boolean retrieveFromCache) throws SystemException {
+		Object[] finderArgs = new Object[] { className, key, owner };
+
+		Object result = null;
+
+		if (retrieveFromCache) {
+			result = FinderCacheUtil.getResult(FINDER_PATH_FETCH_BY_C_K,
+					finderArgs, this);
+		}
+
+		if (result == null) {
+			StringBundler query = new StringBundler(4);
+
+			query.append(_SQL_SELECT_LOCK_WHERE);
+
+			if (className == null) {
+				query.append(_FINDER_COLUMN_C_K_CLASSNAME_1);
+			}
+			else {
+				if (className.equals(StringPool.BLANK)) {
+					query.append(_FINDER_COLUMN_C_K_CLASSNAME_3);
+				}
+				else {
+					query.append(_FINDER_COLUMN_C_K_CLASSNAME_2);
+				}
+			}
+
+			if (key == null) {
+				query.append(_FINDER_COLUMN_C_K_KEY_1);
+			}
+			else {
+				if (key.equals(StringPool.BLANK)) {
+					query.append(_FINDER_COLUMN_C_K_KEY_3);
+				}
+				else {
+					query.append(_FINDER_COLUMN_C_K_KEY_2);
+				}
+			}
+
+			if (owner == null) {
+				query.append(_FINDER_COLUMN_C_K_OWNER_1);
+			}
+			else {
+				if (owner.equals(StringPool.BLANK)) {
+					query.append(_FINDER_COLUMN_C_K_OWNER_3);
+				}
+				else {
+					query.append(_FINDER_COLUMN_C_K_OWNER_2);
+				}
+			}
+
+			String sql = query.toString();
+
+			Session session = null;
+
+			try {
+				session = openSession();
+
+				Query q = session.createQuery(sql);
+
+				QueryPos qPos = QueryPos.getInstance(q);
+
+				if (className != null) {
+					qPos.add(className);
+				}
+
+				if (key != null) {
+					qPos.add(key);
+				}
+
+				if (owner != null) {
+					qPos.add(owner);
+				}
+
+				List<Lock> list = q.list();
+
+				result = list;
+
+				Lock lock = null;
+
+				if (list.isEmpty()) {
+					FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
+						finderArgs, list);
+				}
+				else {
+					lock = list.get(0);
+
+					cacheResult(lock);
+
+					if ((lock.getClassName() == null) ||
+							!lock.getClassName().equals(className) ||
+							(lock.getKey() == null) ||
+							!lock.getKey().equals(key) ||
+							(lock.getOwner() == null) ||
+							!lock.getOwner().equals(owner)) {
+						FinderCacheUtil.putResult(FINDER_PATH_FETCH_BY_C_K,
+							finderArgs, lock);
+					}
+				}
+
+				return lock;
+			}
+			catch (Exception e) {
+				throw processException(e);
+			}
+			finally {
+				if (result == null) {
+					FinderCacheUtil.removeResult(FINDER_PATH_FETCH_BY_C_K,
+						finderArgs);
+				}
+
+				closeSession(session);
+			}
+		}
+		else {
+			if (result instanceof List<?>) {
+				return null;
+			}
+			else {
+				return (Lock)result;
+			}
+		}
+	}
+
+	/**
 	 * Returns all the locks.
 	 *
 	 * @return the locks
@@ -1563,6 +1803,21 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 	public void removeByC_K(String className, String key)
 		throws NoSuchLockException, SystemException {
 		Lock lock = findByC_K(className, key);
+
+		remove(lock);
+	}
+
+	/**
+	 * Removes the lock where className = &#63; and key = &#63; and owner = &#63; from the database.
+	 *
+	 * @param className the class name
+	 * @param key the key
+	 * @param owner the owner
+	 * @throws SystemException if a system exception occurred
+	 */
+	public void removeByC_K(String className, String key, String owner)
+		throws NoSuchLockException, SystemException {
+		Lock lock = findByC_K(className, key, owner);
 
 		remove(lock);
 	}
@@ -1765,6 +2020,106 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 
 				if (key != null) {
 					qPos.add(key);
+				}
+
+				count = (Long)q.uniqueResult();
+			}
+			catch (Exception e) {
+				throw processException(e);
+			}
+			finally {
+				if (count == null) {
+					count = Long.valueOf(0);
+				}
+
+				FinderCacheUtil.putResult(FINDER_PATH_COUNT_BY_C_K, finderArgs,
+					count);
+
+				closeSession(session);
+			}
+		}
+
+		return count.intValue();
+	}
+
+	/**
+	 * Returns the number of locks where className = &#63; and key = &#63; and owner = &#63;.
+	 *
+	 * @param className the class name
+	 * @param key the key
+	 * @param owner the owner
+	 * @return the number of matching locks
+	 * @throws SystemException if a system exception occurred
+	 */
+	public int countByC_K(String className, String key, String owner)
+		throws SystemException {
+		Object[] finderArgs = new Object[] { className, key, owner };
+
+		Long count = (Long)FinderCacheUtil.getResult(FINDER_PATH_COUNT_BY_C_K,
+				finderArgs, this);
+
+		if (count == null) {
+			StringBundler query = new StringBundler(4);
+
+			query.append(_SQL_COUNT_LOCK_WHERE);
+
+			if (className == null) {
+				query.append(_FINDER_COLUMN_C_K_CLASSNAME_1);
+			}
+			else {
+				if (className.equals(StringPool.BLANK)) {
+					query.append(_FINDER_COLUMN_C_K_CLASSNAME_3);
+				}
+				else {
+					query.append(_FINDER_COLUMN_C_K_CLASSNAME_2);
+				}
+			}
+
+			if (key == null) {
+				query.append(_FINDER_COLUMN_C_K_KEY_1);
+			}
+			else {
+				if (key.equals(StringPool.BLANK)) {
+					query.append(_FINDER_COLUMN_C_K_KEY_3);
+				}
+				else {
+					query.append(_FINDER_COLUMN_C_K_KEY_2);
+				}
+			}
+
+			if (owner == null) {
+				query.append(_FINDER_COLUMN_C_K_OWNER_1);
+			}
+			else {
+				if (owner.equals(StringPool.BLANK)) {
+					query.append(_FINDER_COLUMN_C_K_OWNER_3);
+				}
+				else {
+					query.append(_FINDER_COLUMN_C_K_OWNER_2);
+				}
+			}
+
+			String sql = query.toString();
+
+			Session session = null;
+
+			try {
+				session = openSession();
+
+				Query q = session.createQuery(sql);
+
+				QueryPos qPos = QueryPos.getInstance(q);
+
+				if (className != null) {
+					qPos.add(className);
+				}
+
+				if (key != null) {
+					qPos.add(key);
+				}
+
+				if (owner != null) {
+					qPos.add(owner);
 				}
 
 				count = (Long)q.uniqueResult();
@@ -2003,6 +2358,15 @@ public class LockPersistenceImpl extends BasePersistenceImpl<Lock>
 	private static final String _FINDER_COLUMN_C_K_KEY_1 = "lock.key IS NULL";
 	private static final String _FINDER_COLUMN_C_K_KEY_2 = "lock.key = ?";
 	private static final String _FINDER_COLUMN_C_K_KEY_3 = "(lock.key IS NULL OR lock.key = ?)";
+	private static final String _FINDER_COLUMN_C_K_CLASSNAME_1 = "lock.className IS NULL AND ";
+	private static final String _FINDER_COLUMN_C_K_CLASSNAME_2 = "lock.className = ? AND ";
+	private static final String _FINDER_COLUMN_C_K_CLASSNAME_3 = "(lock.className IS NULL OR lock.className = ?) AND ";
+	private static final String _FINDER_COLUMN_C_K_KEY_1 = "lock.key IS NULL AND ";
+	private static final String _FINDER_COLUMN_C_K_KEY_2 = "lock.key = ? AND ";
+	private static final String _FINDER_COLUMN_C_K_KEY_3 = "(lock.key IS NULL OR lock.key = ?) AND ";
+	private static final String _FINDER_COLUMN_C_K_OWNER_1 = "lock.owner IS NULL";
+	private static final String _FINDER_COLUMN_C_K_OWNER_2 = "lock.owner = ?";
+	private static final String _FINDER_COLUMN_C_K_OWNER_3 = "(lock.owner IS NULL OR lock.owner = ?)";
 	private static final String _ORDER_BY_ENTITY_ALIAS = "lock.";
 	private static final String _NO_SUCH_ENTITY_WITH_PRIMARY_KEY = "No Lock exists with the primary key ";
 	private static final String _NO_SUCH_ENTITY_WITH_KEY = "No Lock exists with the key {";

portal-impl/src/com/liferay/portal/upgrade/v6_1_0/util/LockTable.java
@@ -43,7 +43,8 @@ public class LockTable {
 	public static final String TABLE_SQL_DROP = "drop table Lock_";
 
 	public static final String[] TABLE_SQL_ADD_INDEXES = {
-		"create unique index IX_228562AD on Lock_ (className, key_)",
+		"create index IX_228562AD on Lock_ (className, key_)",
+		"create unique index IX_DD635956 on Lock_ (className, key_, owner)",
 		"create index IX_E3F1286B on Lock_ (expirationDate)",
 		"create index IX_13C5CD3A on Lock_ (uuid_)"
 	};

portal-service/src/com/liferay/portal/service/persistence/LockPersistence.java
@@ -346,6 +346,49 @@ public interface LockPersistence extends BasePersistence<Lock> {
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	/**
+	* Returns the lock where className = &#63; and key = &#63; and owner = &#63; or throws a {@link com.liferay.portal.NoSuchLockException} if it could not be found.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @return the matching lock
+	* @throws com.liferay.portal.NoSuchLockException if a matching lock could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public com.liferay.portal.model.Lock findByC_K(java.lang.String className,
+		java.lang.String key, java.lang.String owner)
+		throws com.liferay.portal.NoSuchLockException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	/**
+	* Returns the lock where className = &#63; and key = &#63; and owner = &#63; or returns <code>null</code> if it could not be found. Uses the finder cache.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @return the matching lock, or <code>null</code> if a matching lock could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public com.liferay.portal.model.Lock fetchByC_K(
+		java.lang.String className, java.lang.String key, java.lang.String owner)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	/**
+	* Returns the lock where className = &#63; and key = &#63; and owner = &#63; or returns <code>null</code> if it could not be found, optionally using the finder cache.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @param retrieveFromCache whether to use the finder cache
+	* @return the matching lock, or <code>null</code> if a matching lock could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public com.liferay.portal.model.Lock fetchByC_K(
+		java.lang.String className, java.lang.String key,
+		java.lang.String owner, boolean retrieveFromCache)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	/**
 	* Returns all the locks.
 	*
 	* @return the locks
@@ -417,6 +460,19 @@ public interface LockPersistence extends BasePersistence<Lock> {
 			com.liferay.portal.kernel.exception.SystemException;
 
 	/**
+	* Removes the lock where className = &#63; and key = &#63; and owner = &#63; from the database.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @throws SystemException if a system exception occurred
+	*/
+	public void removeByC_K(java.lang.String className, java.lang.String key,
+		java.lang.String owner)
+		throws com.liferay.portal.NoSuchLockException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	/**
 	* Removes all the locks from the database.
 	*
 	* @throws SystemException if a system exception occurred
@@ -453,6 +509,19 @@ public interface LockPersistence extends BasePersistence<Lock> {
 	* @throws SystemException if a system exception occurred
 	*/
 	public int countByC_K(java.lang.String className, java.lang.String key)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	/**
+	* Returns the number of locks where className = &#63; and key = &#63; and owner = &#63;.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @return the number of matching locks
+	* @throws SystemException if a system exception occurred
+	*/
+	public int countByC_K(java.lang.String className, java.lang.String key,
+		java.lang.String owner)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	/**

portal-service/src/com/liferay/portal/service/persistence/LockUtil.java
@@ -474,6 +474,56 @@ public class LockUtil {
 	}
 
 	/**
+	* Returns the lock where className = &#63; and key = &#63; and owner = &#63; or throws a {@link com.liferay.portal.NoSuchLockException} if it could not be found.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @return the matching lock
+	* @throws com.liferay.portal.NoSuchLockException if a matching lock could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.liferay.portal.model.Lock findByC_K(
+		java.lang.String className, java.lang.String key, java.lang.String owner)
+		throws com.liferay.portal.NoSuchLockException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().findByC_K(className, key, owner);
+	}
+
+	/**
+	* Returns the lock where className = &#63; and key = &#63; and owner = &#63; or returns <code>null</code> if it could not be found. Uses the finder cache.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @return the matching lock, or <code>null</code> if a matching lock could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.liferay.portal.model.Lock fetchByC_K(
+		java.lang.String className, java.lang.String key, java.lang.String owner)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().fetchByC_K(className, key, owner);
+	}
+
+	/**
+	* Returns the lock where className = &#63; and key = &#63; and owner = &#63; or returns <code>null</code> if it could not be found, optionally using the finder cache.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @param retrieveFromCache whether to use the finder cache
+	* @return the matching lock, or <code>null</code> if a matching lock could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.liferay.portal.model.Lock fetchByC_K(
+		java.lang.String className, java.lang.String key,
+		java.lang.String owner, boolean retrieveFromCache)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence()
+				   .fetchByC_K(className, key, owner, retrieveFromCache);
+	}
+
+	/**
 	* Returns all the locks.
 	*
 	* @return the locks
@@ -559,6 +609,21 @@ public class LockUtil {
 	}
 
 	/**
+	* Removes the lock where className = &#63; and key = &#63; and owner = &#63; from the database.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @throws SystemException if a system exception occurred
+	*/
+	public static void removeByC_K(java.lang.String className,
+		java.lang.String key, java.lang.String owner)
+		throws com.liferay.portal.NoSuchLockException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getPersistence().removeByC_K(className, key, owner);
+	}
+
+	/**
 	* Removes all the locks from the database.
 	*
 	* @throws SystemException if a system exception occurred
@@ -604,6 +669,21 @@ public class LockUtil {
 		java.lang.String key)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return getPersistence().countByC_K(className, key);
+	}
+
+	/**
+	* Returns the number of locks where className = &#63; and key = &#63; and owner = &#63;.
+	*
+	* @param className the class name
+	* @param key the key
+	* @param owner the owner
+	* @return the number of matching locks
+	* @throws SystemException if a system exception occurred
+	*/
+	public static int countByC_K(java.lang.String className,
+		java.lang.String key, java.lang.String owner)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().countByC_K(className, key, owner);
 	}
 
 	/**

portal-web/docroot/html/js/liferay/service.js
@@ -142,7 +142,7 @@ Liferay.Service.registerClass(
 Liferay.Service.registerClass(
 	Liferay.Service.Portal, "LayoutSet",
 	{
-		updateLayoutSetPrototypeLink: true,
+		updateLayoutSetPrototypeLinkEnabled: true,
 		updateLookAndFeel: true,
 		updateSettings: true,
 		updateVirtualHost: true

sql/indexes.properties
@@ -398,7 +398,7 @@ IX_C5D69B24=LayoutSetPrototype.Uuid
 
 IX_2932DD37=ListType.Type
 
-IX_228562AD=Lock_.C_K
+IX_DD635956=Lock_.C_K
 IX_E3F1286B=Lock_.LtExpirationDate
 IX_13C5CD3A=Lock_.Uuid
 

sql/indexes.sql
@@ -394,7 +394,8 @@ create index IX_C5D69B24 on LayoutSetPrototype (uuid_);
 
 create index IX_2932DD37 on ListType (type_);
 
-create unique index IX_228562AD on Lock_ (className, key_);
+create index IX_228562AD on Lock_ (className, key_);
+create unique index IX_DD635956 on Lock_ (className, key_, owner);
 create index IX_E3F1286B on Lock_ (expirationDate);
 create index IX_13C5CD3A on Lock_ (uuid_);
 

portal-impl/test/com/liferay/portal/service/persistence/LockPersistenceTest.java
@@ -226,6 +226,13 @@ public class LockPersistenceTest extends BasePersistenceTestCase {
 				existingLockModelImpl.getOriginalClassName()));
 		assertTrue(Validator.equals(existingLockModelImpl.getKey(),
 				existingLockModelImpl.getOriginalKey()));
+
+		assertTrue(Validator.equals(existingLockModelImpl.getClassName(),
+				existingLockModelImpl.getOriginalClassName()));
+		assertTrue(Validator.equals(existingLockModelImpl.getKey(),
+				existingLockModelImpl.getOriginalKey()));
+		assertTrue(Validator.equals(existingLockModelImpl.getOwner(),
+				existingLockModelImpl.getOriginalOwner()));
 	}
 
 	protected Lock addLock() throws Exception {