zxdgoal/liferay-portal

commit c2f5a11dc55079341db075faea5bcc97280629e4

Minor refactoring
git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@7275 05bdf26c-840f-0410-9ced-eb539d925f36

portal-impl/src/com/liferay/portal/im/AIMConnector.java
@@ -22,16 +22,21 @@
 
 package com.liferay.portal.im;
 
-import com.levelonelabs.aim.AIMBuddy;
-import com.levelonelabs.aim.AIMClient;
-
 import com.liferay.portal.util.PropsUtil;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.walluck.oscar.AIMConnection;
+import org.walluck.oscar.AIMSession;
+import org.walluck.oscar.client.Oscar;
 
 /**
  * <a href="AIMConnector.java.html"><b><i>View Source</i></b></a>
  *
  * @author Brian Wing Shun Chan
  * @author Brett Randall
+ * @author Bruno Farache
  *
  */
 public class AIMConnector {
@@ -53,33 +58,49 @@ public class AIMConnector {
 		String login = PropsUtil.get(PropsUtil.AIM_LOGIN);
 		String password = PropsUtil.get(PropsUtil.AIM_PASSWORD);
 
-		_aim = new AIMClient(login, password);
+		AIMSession ses = new AIMSession();
 
-		_aim.signOn();
+		ses.setSN(login);
+
+		Oscar oscar = new Oscar();
+
+		oscar.setSN(login);
+		oscar.setPassword(password);
+
+		sess.init();
 	}
 
 	private void _disconnect() {
 		if (_aim != null) {
-			_aim.signOff();
+			AIMConnection.killAllInSess(_aim);
 		}
 	}
 
 	private void _send(String to, String msg) {
-		if (_aim == null) {
-			_connect();
+		try {
+			if (_aim == null) {
+				_connect();
+
+				// Daim's listeners are buggy. Instead, just wait a second
+				// before sending the first message.
+
+				Thread.sleep(1000);
+			}
+
+			_oscar.sendIM(_aim, to, msg, Oscar.getICQCaps());
 		}
-
-		AIMBuddy buddy = new AIMBuddy(to);
-
-		buddy.setOnline(true);
-
-		_aim.addBuddy(buddy);
-
-		_aim.sendMessage(buddy, msg);
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Could not send AIM message");
+			}
+		}
 	}
 
 	private static AIMConnector _instance = new AIMConnector();
 
-	private AIMClient _aim;
+	private static Log _log = LogFactory.getLog(AIMConnector.class);
+
+	private AIMSession _aim;
+	private Oscar _oscar;
 
 }

.classpath
@@ -84,6 +84,7 @@
 	<classpathentry kind="lib" path="lib/portal/compass.jar"/>
 	<classpathentry kind="lib" path="lib/portal/concurrent.jar"/>
 	<classpathentry kind="lib" path="lib/portal/crypt.jar"/>
+	<classpathentry kind="lib" path="lib/portal/daim.jar"/>
 	<classpathentry kind="lib" path="lib/portal/displaytag.jar"/>
 	<classpathentry kind="lib" path="lib/portal/dom4j.jar"/>
 	<classpathentry kind="lib" path="lib/portal/easyconf.jar"/>
@@ -102,7 +103,6 @@
 	<classpathentry kind="lib" path="lib/portal/jackrabbit-core.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jackrabbit-jcr-commons.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jackrabbit-text-extractors.jar"/>
-	<classpathentry kind="lib" path="lib/portal/jaim.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jaxen.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jaxrpc.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jazzy.jar"/>

tools/ext_tmpl/.classpath
@@ -68,6 +68,7 @@
 	<classpathentry kind="lib" path="lib/portal/compression-filter.jar"/>
 	<classpathentry kind="lib" path="lib/portal/concurrent.jar"/>
 	<classpathentry kind="lib" path="lib/portal/crypt.jar"/>
+	<classpathentry kind="lib" path="lib/portal/daim.jar"/>
 	<classpathentry kind="lib" path="lib/portal/displaytag.jar"/>
 	<classpathentry kind="lib" path="lib/portal/dom4j.jar"/>
 	<classpathentry kind="lib" path="lib/portal/doubleclick-filter.jar"/>
@@ -86,7 +87,6 @@
 	<classpathentry kind="lib" path="lib/portal/jackrabbit-core.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jackrabbit-jcr-commons.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jackrabbit-text-extractors.jar"/>
-	<classpathentry kind="lib" path="lib/portal/jaim.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jaxen.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jaxrpc.jar"/>
 	<classpathentry kind="lib" path="lib/portal/jazzy.jar"/>

commit 3069e27923c92e32ffd0e92715b90b7c4e688565

LPS-49294 I think this is what it meant to be. It was originally caused by c2f5a11dc55079341db075faea5bcc97280629e4 without any ticket info.

portal-impl/src/com/liferay/portal/im/AIMConnector.java
@@ -51,10 +51,10 @@ public class AIMConnector {
 
 		ses.setSN(login);
 
-		Oscar oscar = new Oscar();
+		_oscar = new Oscar();
 
-		oscar.setSN(login);
-		oscar.setPassword(password);
+		_oscar.setSN(login);
+		_oscar.setPassword(password);
 
 		ses.init();
 	}
@@ -90,6 +90,6 @@ public class AIMConnector {
 	private static final AIMConnector _instance = new AIMConnector();
 
 	private AIMSession _aim;
-	private final Oscar _oscar = null;
+	private Oscar _oscar;
 
 }